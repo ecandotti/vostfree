f3d11ac144261cd3f09590d42d4ab984029cc585
ecandotti <enzo.candotti16@hotmail.com>
2022-03-19 20:50:13 +0100

    update mock

---
+++ src/utils/animock.ts
@@ -0,0 +1,173 @@
+export const AniMock = [
+    {
+        id: '001',
+        title: 'One Piece',
+        author: 'Enzo Candotti',
+        thumbnail: {
+            path: 'https://i.pinimg.com/474x/8a/52/ae/8a52aeea03d6e43c60106ded2f428957',
+            extension: '.jpg',
+        },
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+        like: 3,
+        dislike: 2,
+        videos: {
+            saisons: [
+                [
+                    { id: 'aaaa', path: '', title: '', thumbnail: { path: '', extension: '' } },
+                    { id: 'iii', path: '', title: '', thumbnail: { path: '', extension: '' } },
+                    { id: 'uytgfcxs', path: '', title: '', thumbnail: { path: '', extension: '' } },
+                    { id: 'wqswq', path: '', title: '', thumbnail: { path: '', extension: '' } },
+                ],
+            ],
+        },
+    },
+    {
+        id: '5690',
+        title: 'Naruto',
+        author: 'Enzo Candotti',
+        thumbnail: {
+            path: 'https://i.pinimg.com/originals/6c/0d/6e/6c0d6e4ae8d244915ba5a37fd56b6a1b',
+            extension: '.jpg',
+        },
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+        like: 5,
+        dislike: 2,
+        videos: {
+            saisons: [
+                [
+                    {
+                        number: 1,
+                        id: 'qqq',
+                        path: '',
+                        title: 'Naruto Uzumaki',
+                        thumbnail: {
+                            path: 'https://fr.web.img3.acsta.net/r_654_368/newsv7/21/02/24/09/05/3123179',
+                            extension: '.jpg',
+                        },
+                    },
+                    {
+                        number: 2,
+                        id: 'sss',
+                        path: '',
+                        title: 'Rasengan !',
+                        thumbnail: {
+                            path: 'https://cdn.shopify.com/s/files/1/0046/2779/1960/articles/rasengan_naruto_shippuden_640x',
+                            extension: '.jpg',
+                        },
+                    },
+                    {
+                        number: 3,
+                        id: 'ddddd',
+                        path: '',
+                        title: 'Kyubi',
+                        thumbnail: {
+                            path: 'https://pm1.narvii.com/6954/b8c128c4202170d37b7485a042162f72bc8d7e00r1-1891-1080v2_hq',
+                            extension: '.jpg',
+                        },
+                    },
+                    {
+                        number: 4,
+                        id: 'ffff',
+                        path: '',
+                        title: 'Naruto vs Sasuke',
+                        thumbnail: {
+                            path: 'https://animehighlight.news/images/large/2021-naruto-vs-sasuke-comparaison-des-pouvoirs-jusqua-la-serie-boruto-1618584614',
+                            extension: '.jpeg',
+                        },
+                    },
+                ],
+            ],
+        },
+    },
+    {
+        id: '3',
+        title: 'Kimetsu No Yahiba',
+        author: 'Enzo Candotti',
+        thumbnail: {
+            path: 'https://www.manga-news.com/public/images/pix/dvd/3677/demon-slayers-anime-visual-2',
+            extension: '.jpg',
+        },
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+        like: 4,
+        dislike: 2,
+        videos: {
+            saisons: [
+                [
+                    { id: 'gfsfwx', path: '', title: '', thumbnail: { path: '', extension: '' } },
+                    { id: 'hju', path: '', title: '', thumbnail: { path: '', extension: '' } },
+                    { id: 'uyt', path: '', title: '', thumbnail: { path: '', extension: '' } },
+                    { id: 'vfr', path: '', title: '', thumbnail: { path: '', extension: '' } },
+                ],
+            ],
+        },
+    },
+    {
+        id: '7908',
+        title: 'My Heroes Academia',
+        author: 'Enzo Candotti',
+        thumbnail: {
+            path: 'https://images-na.ssl-images-amazon.com/images/I/51kV0m6eVvL._SX326_BO1,204,203,200_',
+            extension: '.jpg',
+        },
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+        like: 4,
+        dislike: 2,
+        videos: {
+            saisons: [
+                [
+                    { id: 'rtynb', path: '', title: '', thumbnail: { path: '', extension: '' } },
+                    { id: 'gvrd', path: '', title: '', thumbnail: { path: '', extension: '' } },
+                    { id: 'uun', path: '', title: '', thumbnail: { path: '', extension: '' } },
+                    { id: 'trze', path: '', title: '', thumbnail: { path: '', extension: '' } },
+                ],
+            ],
+        },
+    },
+    {
+        id: '87',
+        title: 'Parasyte',
+        author: 'Enzo Candotti',
+        thumbnail: {
+            path: 'https://i.pinimg.com/564x/12/3b/b5/123bb5902da8a34aac43183d8d39c2bc--kiseijuu-paintings',
+            extension: '.jpg',
+        },
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+        like: 3,
+        dislike: 2,
+        videos: {
+            saisons: [
+                [
+                    { id: 'zae', path: '', title: '', thumbnail: { path: '', extension: '' } },
+                    { id: 'fsdc', path: '', title: '', thumbnail: { path: '', extension: '' } },
+                    { id: 'csdbt', path: '', title: '', thumbnail: { path: '', extension: '' } },
+                    { id: 'byrty', path: '', title: '', thumbnail: { path: '', extension: '' } },
+                ],
+            ],
+        },
+    },
+    {
+        id: '132',
+        title: 'Shinegeki No Kyogin',
+        author: 'Enzo Candotti',
+        thumbnail: { path: 'https://pbs.twimg.com/media/D67RkOlXkAI16un', extension: '.jpg' },
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+        like: 5,
+        dislike: 2,
+        videos: {
+            saisons: [
+                [
+                    { id: 'aze', path: '', title: '', thumbnail: { path: '', extension: '' } },
+                    { id: 'zer', path: '', title: '', thumbnail: { path: '', extension: '' } },
+                    { id: 'ert', path: '', title: '', thumbnail: { path: '', extension: '' } },
+                    { id: 'rty', path: '', title: '', thumbnail: { path: '', extension: '' } },
+                ],
+            ],
+        },
+    },
+]
