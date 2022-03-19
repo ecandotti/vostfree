0179614498b039e081fe0f2e51d418d43eabba0d
ecandotti <enzo.candotti16@hotmail.com>
2022-03-19 20:48:34 +0100

    comment splash screen function

--- App.tsx
+++ App.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+// import SplashScreen from 'react-native-splash-screen'
 
 import { AuthContextProvider } from '@configs/contexts/AuthContext'
 import StackNavigator from '@configs/routes'
 
 const App: React.FC = () => {
+    // useEffect(() => {
+    //     SplashScreen.hide()
+    // })
+
     return (
         <AuthContextProvider>
             <StackNavigator />
