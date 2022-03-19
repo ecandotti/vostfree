fb5756e8ba41a0460c4152620382fd31b057d8e3
ecandotti <enzo.candotti16@hotmail.com>
2022-03-19 20:50:26 +0100

    display watchlist

--- src/screens/selfcare/Watchlist/index.tsx
+++ src/screens/selfcare/Watchlist/index.tsx
@@ -1,14 +1,26 @@
-import React from 'react'
-import { Text } from 'react-native'
+import React, { useContext } from 'react'
+import { FlatList } from 'react-native'
 
-import { Container } from '@components/styled-components'
+import AnimeContext from '@configs/contexts/AnimeContext'
 
 import { ScreenTypes } from 'types/ScreenTypes'
 
+import { Container } from '@components/styled-components'
+import Card from '@components/Card'
+
 const Index: React.FC<ScreenTypes> = () => {
+    const { watchlist } = useContext(AnimeContext)
+
     return (
         <Container>
-            <Text>Hello Watchlist</Text>
+            <FlatList
+                data={watchlist}
+                renderItem={({ item }: any) => <Card item={item} />}
+                style={{ marginTop: 10 }}
+                keyExtractor={(item: any) => item.id}
+                showsHorizontalScrollIndicator={false}
+                horizontal
+            />
         </Container>
     )
 }
