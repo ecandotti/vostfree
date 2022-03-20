2f2b892f26f6ad37bafeaef7df97569cb827bf30
ecandotti <enzo.candotti16@hotmail.com>
2022-03-20 01:48:03 +0100

    add splash screen android

--- App.tsx
+++ App.tsx
@@ -1,13 +1,13 @@
 import React, { useEffect } from 'react'
-// import SplashScreen from 'react-native-splash-screen'
+import SplashScreen from 'react-native-splash-screen'
 
 import { AuthContextProvider } from '@configs/contexts/AuthContext'
 import StackNavigator from '@configs/routes'
 
 const App: React.FC = () => {
-    // useEffect(() => {
-    //     SplashScreen.hide()
-    // })
+    useEffect(() => {
+        SplashScreen.hide()
+    })
 
     return (
         <AuthContextProvider>
